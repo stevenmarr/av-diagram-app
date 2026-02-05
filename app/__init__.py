56916b8f00b4f78d07ecec497b955fba45871863
Steven Marr <stevenmarr@me.com>
2026-02-05 09:15:33 -0600

    add super_admin_bp

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,8 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
+    from app.controllers.super_admin import super_admin_bp
+    app.register_blueprint(super_admin_bp)
     from app.controllers.auth import auth_bp
     app.register_blueprint(auth_bp)
     # Temporary test route (returns 200 instead of 404)
